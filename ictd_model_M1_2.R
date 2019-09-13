76b5b3d55c52d578123dcdc58f06996c857af54d
changwn <changwn.nku@gmail.com>
2019-09-13 19:11:18 -0400

    Add files via upload

--- ictd_model_M1_2.R
+++ ictd_model_M1_2.R
@@ -766,6 +766,12 @@ ICTD_round1 <- function(data_bulk)
   {
     if(length(tg_markers[[i]])==1)
     {
+      tg_ccc<-intersect(rownames(data01),LM22_test_list1_plus_unique_core[[tg_ids[i]]])
+      print("debug 111")
+      print(i)
+      print(tg_ccc)
+      print(sum(is.na(data01[tg_ccc,])))
+      print(c(min(data01[tg_ccc,]),max(data01[tg_ccc,])))
       tg_markers[[i]]<-top5_SVD_cor(data01,LM22_test_list1_plus_unique_core[[tg_ids[i]]]) ##################################################
     }
   }
