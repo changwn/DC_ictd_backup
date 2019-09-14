22eb73fffb40ca5cc4f0e8a3fc298e1cd55ab7f4
changwn <changwn.nku@gmail.com>
2019-09-13 23:12:27 -0400

    Add files via upload

--- test_random.R
+++ test_random.R
@@ -35,35 +35,35 @@ ictd_2_output <- function(ictd_prop, dataset.name)
   return(output_df)
 }
 
-ictd_2_output_real <- function(ictd_prop, dataset.name)
-{
-  #2
-  col_name <- matrix(colnames(ictd_prop),1,length(colnames(ictd_prop)))
-  vv <- c()
-  for(i in 1:length(col_name))
-  {
-    vv <- c(vv, rep(col_name[i], 8))  #8 cell types
-  }
-  sample_col <- vv
-  
-  #3  
-  celltype_col <- rep(c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts'), ncol(ictd_prop))
-  #4
-  #prediction_col <- rep(runif(8,min=0.01,max=0.8), ncol(ictd_prop))  
-  prediction_col <- c()
-  cell_type_order <- c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts')
-  for(i in 1:length(col_name))
-  {
-    prop_tmp <- ictd_prop[cell_type_order,i]
-    prediction_col <- c(prediction_col, prop_tmp)
-  }
-  
-  
-  #bind
-  output_df <- data.frame(dataset.name=dataset.name,sample.id=sample_col,cell.type=celltype_col,prediction=prediction_col)
-  
-  return(output_df)
-}
+# ictd_2_output_real <- function(ictd_prop, dataset.name)
+# {
+#   #2
+#   col_name <- matrix(colnames(ictd_prop),1,length(colnames(ictd_prop)))
+#   vv <- c()
+#   for(i in 1:length(col_name))
+#   {
+#     vv <- c(vv, rep(col_name[i], 8))  #8 cell types
+#   }
+#   sample_col <- vv
+#   
+#   #3  
+#   celltype_col <- rep(c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts'), ncol(ictd_prop))
+#   #4
+#   #prediction_col <- rep(runif(8,min=0.01,max=0.8), ncol(ictd_prop))  
+#   prediction_col <- c()
+#   cell_type_order <- c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts')
+#   for(i in 1:length(col_name))
+#   {
+#     prop_tmp <- ictd_prop[cell_type_order,i]
+#     prediction_col <- c(prediction_col, prop_tmp)
+#   }
+#   
+#   
+#   #bind
+#   output_df <- data.frame(dataset.name=dataset.name,sample.id=sample_col,cell.type=celltype_col,prediction=prediction_col)
+#   
+#   return(output_df)
+# }
 
 
 #-------------------------------
@@ -82,6 +82,7 @@ input_df <- read.csv('input/input.csv')
 print('read <input.csv> done!')
 
 
+
 # Extract the names of each dataset
 dataset_name <- as.character(input_df$dataset.name)
 
@@ -98,15 +99,15 @@ for(i in 1:length(expression_files))
   rownames(data_tmp) <- data_tmp[,1]
   data_tmp <- data_tmp[,-1]
   #ictd_result <- ICTD(data_tmp)
-  #ictd_prop <- data_tmp[1:12,]
-  ictd_prop <- matrix(runif(8*ncol(data_tmp), 0.1,0.4), 8, ncol(data_tmp))
-  colnames(ictd_prop) <- paste("Setsample", 1:ncol(ictd_prop),sep = "")
-  rownames(ictd_prop) <- c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts')
+  ictd_prop <- data_tmp[1:12,]
+  #ictd_prop <- matrix(runif(8*ncol(data_tmp), 0.1,0.4), 8, ncol(data_tmp))
+  #colnames(ictd_prop) <- paste("Setsample", 1:ncol(ictd_prop),sep = "")
+  #rownames(ictd_prop) <- c('CD4.T.cells','CD8.T.cells','NK.cells','B.cells','monocytic.lineage','neutrophils','endothelial.cells','fibroblasts')
   
   dn_tmp <- unlist(strsplit(expression_files[i],split='.',fixed=T))[[1]]
   
-  #output_tmp <- ictd_2_output(ictd_prop, dn_tmp)
-  output_tmp <- ictd_2_output_real(ictd_prop, dn_tmp)
+  output_tmp <- ictd_2_output(ictd_prop, dn_tmp)
+  #output_tmp <- ictd_2_output_real(ictd_prop, dn_tmp)
   #combine prediction into big dataframe
   output_all_ds <- rbind(output_all_ds, output_tmp)
   
